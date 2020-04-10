f3b8acf92161308b5d496c6a9e2e1be2dbc83929
Sebastian Skałacki <skalee@gmail.com>
2020-04-10 17:21:24 +0200

    Preprocess input files in order to fix term ids
    Term ids in IEV should meet format XXX-XX-XX, which is ensured by data
    files preprocessor.

---
+++ _plugins/concept_preprocessor.rb
@@ -0,0 +1,23 @@
+# (c) Copyright 2020 Ribose Inc.
+#
+
+require "jekyll/geolexica/concepts_generator"
+
+module GeolexicaOverrides
+  def read_concept_file(path)
+    # binding.pry
+    format_termid = lambda do |t|
+      t.to_s.rjust(7, "0").insert(3, "-").insert(6, "-")
+    end
+
+    concept_hash = super(path)
+    concept_hash["termid"] = format_termid.(concept_hash["termid"])
+    concept_hash.values.each do |val|
+      next unless val.kind_of?(Hash) && val.key?("id")
+      val["id"] = format_termid.(val["termid"])
+    end
+    concept_hash
+  end
+end
+
+Jekyll::Geolexica::ConceptsGenerator.prepend(GeolexicaOverrides)
